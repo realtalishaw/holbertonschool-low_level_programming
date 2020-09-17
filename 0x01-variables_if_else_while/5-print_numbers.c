11853dcb65aeafbf95b725f5068f0338a81f0ac4
realtalishaw <misstalishawhite@gmail.com>
2020-09-17 10:29:13 +0000

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main- Short description, single line
+ * Description: Longer description of the function)?
+ * section header: Section description)
+ * Return: Description of the returned value
+ */
+
+int main(void)
+{
+	char n = '0';
+
+	for (; n  <= '9'; n++)
+	{
+		putchar(n);
+		}
+	putchar('\n');
+	return (0);
+}
