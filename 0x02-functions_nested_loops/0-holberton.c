3e012f3b256c952fc4918348f7c59f497ac7fffb
realtalishaw <misstalishawhite@gmail.com>
2020-09-21 13:26:09 +0000

    _putchar

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+
+int main(void)
+{
+	char H [] = "Holberton";
+	int i;
+
+	for (i = 0; H[i] != 0; i++)
+		_putchar(H[i]);
+	_putchar('\n');
+	return (0);
+}
