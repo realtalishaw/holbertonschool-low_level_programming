fa9f23a87a179c3be18940ba89dc46b478e87377
realtalishaw <misstalishawhite@gmail.com>
2020-09-22 14:25:51 +0000

    updated

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,7 +1,4 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
-
-void _puchar();
+void _putchar();
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -9,5 +6,3 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
-
-#endif
