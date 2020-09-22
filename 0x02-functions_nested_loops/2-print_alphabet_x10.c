9ca93192ad330625efb303b618449f02263304ac
realtalishaw <misstalishawhite@gmail.com>
2020-09-22 14:27:31 +0000

    uploading file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_alphabet_x10 - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	int a;
+	int n;
+
+	for(n = '0'; n <= '9'; n++)
+		{
+			for(a = 'a'; a <= 'z'; a++)
+			{
+				_putchar(a);
+			}
+			_putchar('\n');
+		}
+}
