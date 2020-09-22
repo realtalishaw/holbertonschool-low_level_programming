f38a1cd5a04e4d23679a0de05c29f4d4a605f154
realtalishaw <misstalishawhite@gmail.com>
2020-09-22 16:23:13 +0000

    flip the math

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+void jack_bauer(void)
+{
+	int hour;
+	int min;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (min = 0; min < 60; min++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour %  10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
