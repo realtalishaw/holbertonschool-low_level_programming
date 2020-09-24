3eecd94fd171ace893d683ab7e1293bd3724f079
realtalishaw <misstalishawhite@gmail.com>
2020-09-24 20:50:46 +0000

    What if there was no if

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -11,10 +11,7 @@ void print_line(int n)
 {
 	int a;
 
-	for (a = 0; a <= n; a++)
-	{
-		if (n > 0)
-			_putchar('_');
-	_putchar('\n');
-	}
+	for (a = 0; a < n; a++)
+		_putchar('_');
+       _putchar('\n');
 }
