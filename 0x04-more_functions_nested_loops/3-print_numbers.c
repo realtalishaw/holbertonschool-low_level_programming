ab1e1951a8190858c99600b8206ff8c94af8add2
realtalishaw <misstalishawhite@gmail.com>
2020-09-24 15:10:41 +0000

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - ekjfsl
+ *
+ * Return: jfaljfla
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+		_putchar(n);
+
+	_putchar('\n');
+}
