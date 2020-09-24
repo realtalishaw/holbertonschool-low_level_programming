cdd5371e2ca5bcf72ae8cf8e08869a8db29e5764
realtalishaw <misstalishawhite@gmail.com>
2020-09-24 16:26:34 +0000

    Fizz Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - fizz
+ *
+ * Return: fkj;lda
+ */
+
+int main (void)
+{
+	int n;
+
+
+	for (n = 1; n <= 100; n++)
+	{
+		if ( n % 3 == 0)
+			printf("Fizz ");
+
+		else if ( n % 5 == 0)
+			printf("Buzz ");
+		else if ( n % 15 == 0)
+			printf("FizzBuzz ");
+		else
+			printf("%d ", n);
+	}
+	putchar('\n');
+	return (0);
+}
