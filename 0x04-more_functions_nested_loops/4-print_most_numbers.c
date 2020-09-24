a4a30f70e862c1d7bad899d352513db80a254e8b
realtalishaw <misstalishawhite@gmail.com>
2020-09-24 15:17:03 +0000

    I believe in numbers and signs

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - kfejdklfs
+ *
+ * Return: dhsj
+ */
+
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		if (n != '2' && n != '4')
+			_putchar(n);
+	}
+	_putchar('\n');
+}
