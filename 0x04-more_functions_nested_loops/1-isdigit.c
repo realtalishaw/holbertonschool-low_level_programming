1ba5e71b24ae95444a6e32ed4daa925ad7ce0095
realtalishaw <misstalishawhite@gmail.com>
2020-09-24 15:02:32 +0000

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _isdigit - ekjfsl
+ * @c: fdfaf
+ *
+ * Return: jfaljfla
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
