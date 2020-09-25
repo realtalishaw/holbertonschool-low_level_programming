90d0d889c4fe9c78f3c2fc8799c93ac301a71eb5
realtalishaw <misstalishawhite@gmail.com>
2020-09-25 01:28:46 +0000

    added if

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,10 +12,15 @@ void print_square(int size)
 	int a;
 	int b;
 
-	for (a = 0; a < size; a++)
+	if (size > 0)
 	{
-		for (b = 0; b < size; b++)
-			_putchar('#');
-		_putchar('\n');
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < size; b++)
+				_putchar('#');
+			_putchar('\n');
+		}
 	}
+	else
+		_putchar('\n');
 }
