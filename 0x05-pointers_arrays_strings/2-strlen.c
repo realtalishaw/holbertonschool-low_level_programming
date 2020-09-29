549076666d7b834b698f996130b58f6548de13ac
realtalishaw <misstalishawhite@gmail.com>
2020-09-29 15:00:20 +0000

    This report...

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _strlen - fdk;slf
+ * @s: djijf
+ *
+ * Return: jadkf
+ */
+
+int _strlen(char *s)
+{
+	int count = 0;
+	int i;
+
+	for (i = 0; s[i] != 0; i++)
+		count++;
+	return (count);
+}
