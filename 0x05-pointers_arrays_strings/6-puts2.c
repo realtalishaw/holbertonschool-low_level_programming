6b99f848e487e6f13288f14a9b81f43d86f51b97
realtalishaw <misstalishawhite@gmail.com>
2020-09-29 15:54:16 +0000

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * puts2 - geknlfls
+ * @str: string
+ *
+ * Return: efj
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != 0; i += 2)
+		_putchar(str[i]);
+	_putchar('\n');
+}
