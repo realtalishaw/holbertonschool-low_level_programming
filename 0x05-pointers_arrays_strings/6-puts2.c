7b82dc5c543b54b393b4f8e9b4bf92eac6d764f5
realtalishaw <misstalishawhite@gmail.com>
2020-09-29 16:18:11 +0000

    NULL isn't void

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
  * puts2 - geknlfls
@@ -11,7 +12,12 @@ void puts2(char *str)
 {
 	int i;
 
+	if (str != NULL)
+	{
 	for (i = 0; str[i] != 0; i += 2)
 		_putchar(str[i]);
 	_putchar('\n');
+	}
+	else
+		_putchar('\n');
 }
