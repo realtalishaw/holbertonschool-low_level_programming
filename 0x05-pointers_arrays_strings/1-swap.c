7da49f00aa79f0a9f006a2294af8a7dd9d5387f8
realtalishaw <misstalishawhite@gmail.com>
2020-09-29 14:41:34 +0000

    Trading places

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * swap_int - swao
+ * @a: iglkdsj
+ * @b: fhdhla
+ *
+ * Return: sknda
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
