b758b99389c1cce2ce6154f7087db5a56981df67
realtalishaw <misstalishawhite@gmail.com>
2020-09-29 15:07:02 +0000

    I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _puts - fealjf
+ * @str: jgjsd
+ *
+ * Return: dghoai'jg
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != 0; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
