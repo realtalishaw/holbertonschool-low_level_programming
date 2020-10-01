3ff3b1e2b67d9e79713995e378f9a59614a14073
realtalishaw <misstalishawhite@gmail.com>
2020-10-01 22:30:49 +0000

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
  * _strcmp - gks
@@ -10,14 +11,16 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i;
 
-	for (; *s1 == *s2; s1++, s2++)
+	for(;(*s1 != 0 && *s2 != 0) && *s1 == *s2; s1++, s2++)
+	{
+	printf("s1: %c\n", *s1);
+	printf("s2: %c\n", *s2);
+			   
 		if (*s1 == 0)
 			return (0);
-	i = (s1 - s2);
-	if (s1 < s2)
-		return (i + 1);
-	else
-		return (i - 1);
+	}
+	printf("us1: %c\n", *s1);
+	printf("us2: %c\n", *s2);
+       return (*s2 - *s1);
 }
