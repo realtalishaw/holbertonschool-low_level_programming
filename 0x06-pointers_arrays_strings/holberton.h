674bc1b73af6776f48269b8bcb97116394def973
realtalishaw <misstalishawhite@gmail.com>
2020-10-01 18:08:46 +0000

    header file

---
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -0,0 +1,8 @@
+int _strcmp(char *s1, char *s2);
+char *_strcat(char *dest, char *src);
+char *_strncpy(char *dest, char *src, int n);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
