a734010ee2f92c23a7e85bfa2c5aa20546567f7b
realtalishaw <misstalishawhite@gmail.com>
2020-10-02 02:21:48 +0000

    Incorrect only capitalizes the word

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * cap_string - kjdadfa
+ * @s: fjdkljfs
+ *
+ * Return: sofjs
+ */
+
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != 0; i++)
+	{
+		if (i == 0)
+		{
+			if (s[i] >= 97 && s[i] <= 122)
+						s[i] = s[i] - 32;
+		}
+	}
+	return (s);
+}
