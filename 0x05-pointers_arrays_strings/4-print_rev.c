cf0d663ba2e7d5c5ce9afdb32a535a1686b3ffa0
realtalishaw <misstalishawhite@gmail.com>
2020-10-03 22:09:59 +0000

    I can only go one way.

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+* print_rev - ekdjlsk
+* @s: ejsekf
+*
+* Return: fjkhdsk
+*/
+
+void print_rev(char *s)
+{
+	int len, i;
+
+	for (len  = 0; s[len]; len++)
+	{
+		;
+
+	}
+	for (i = len; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
