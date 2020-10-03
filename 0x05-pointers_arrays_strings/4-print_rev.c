0bed60464fa340c5cb2f5dc2910ea75706d41fba
realtalishaw <misstalishawhite@gmail.com>
2020-10-03 22:27:14 +0000

    forgot to remove debugging code for checker

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -13,10 +13,9 @@ void print_rev(char *s)
 
 	for (len  = 0; s[len] != 0; len++)
 	{
-		_putchar(s[len]);
+		;
 
 	}
-	_putchar('\n');
 	for (i = len - 1; i >= 0; i--)
 		_putchar(s[i]);
 	_putchar('\n');
