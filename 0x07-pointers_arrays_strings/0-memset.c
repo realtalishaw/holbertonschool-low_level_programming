a02d69b5301955d247fc16a9ea27b59948369384
realtalishaw <misstalishawhite@gmail.com>
2020-10-05 13:30:04 +0000

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _memset - efjs
+ * @s: fkjds
+ * @b: fkjd
+ * @n: fkds
+ *
+ * Return: fekj
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for ( i = 0; i < n; i++)
+		s[i] = b;
+
+	return (s);
+}
