9d52ca1ecd4031c62dd04cf028d63c3bd73cce85
realtalishaw <misstalishawhite@gmail.com>
2020-10-05 15:58:38 +0000

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strspn - dfgddf
+ * @s: ffdsfds
+ * @accept: fdsds
+ *
+ * Return: dsfd
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	char c, *p, *str;
+
+	for (str = s, c = *str; c != 0; str++, c = *str)
+	{
+		for (p = accept; *p != 0; p++)
+		{
+			if (c == *p)
+			{
+				goto next;
+			}
+		}
+		break;
+	next: ;
+	}
+	return (str - s);
+}
