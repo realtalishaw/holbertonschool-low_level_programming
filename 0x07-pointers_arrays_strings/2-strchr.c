478d0a9057dc610dfee33c78d63ca2f26b70ca5d
realtalishaw <misstalishawhite@gmail.com>
2020-10-05 14:06:16 +0000

    locates a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _strchr - fdskhkj
+ * @c: fdjfsdf
+ * @s: ghjljlk
+ *
+ * Return: fds
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i]; s++)
+		if (s[i] == c)
+			return (s);
+	return (s);
+}
