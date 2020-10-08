322b00c79b01e520e6f11eeb675160a4b28c4ee0
realtalishaw <misstalishawhite@gmail.com>
2020-10-08 04:15:03 +0000

    is a prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * is_prime_number - fssad
+ * @n: fsads
+ *
+ * Return: idk
+ */
+
+int is_prime_number(int n)
+{
+
+	if (
+		return (is_prime_number(n));
+	if (n % 1 == 0)
+		return (0);
+}
