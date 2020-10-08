25b15258ddcf52f45605a9e2f7fa6fc2985caa93
realtalishaw <misstalishawhite@gmail.com>
2020-10-08 02:49:05 +0000

    strlen

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _strlen_recursion - fdjfsfs
+ * @s: fdsfd
+ *
+ * Return: fdsdf
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == 0)
+		return (0);
+	s++;
+	return (1 + _strlen_recursion(S));
+}
