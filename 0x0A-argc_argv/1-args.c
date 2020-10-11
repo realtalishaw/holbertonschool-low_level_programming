0431b59a0801c288ed919a4a1ded05b2f23c723a
realtalishaw <misstalishawhite@gmail.com>
2020-10-11 17:17:09 +0000

    nargs

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - fsfd
+ * @argc: dsas
+ * @argv: fdasa
+ *
+ * Return: dsas
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc -1);
+	return (0);
+}
