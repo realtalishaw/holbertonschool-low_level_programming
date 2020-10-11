6fa0e0ae698a70521a5b71c6269c5be2898e41f0
realtalishaw <misstalishawhite@gmail.com>
2020-10-11 17:23:18 +0000

    The best argument...

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - fdsfds
+ * @argc: dadfa
+ * @argv: fdada
+ *
+ * Return: sas
+ */
+
+int main(int argc, char *argv[])
+{
+	while (argc--)
+		printf("%s\n", *argv++);
+	return (0);
+}
