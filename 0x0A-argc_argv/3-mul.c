17f3dc9d09f380a382cba0e0540be245bc6ac1c2
realtalishaw <misstalishawhite@gmail.com>
2020-10-11 17:37:14 +0000

    fixed the syntax

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,12 +12,10 @@
 int main(int argc, char *argv[])
 {
 	if (argc == 3)
-	{
-		printf("%d\n",(atoi(argv[1]) * (atoi(argv[2]))));
-		else
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	else
 		{
 			printf("Error\n");
 		}
-	}
 			return (0);
 }
