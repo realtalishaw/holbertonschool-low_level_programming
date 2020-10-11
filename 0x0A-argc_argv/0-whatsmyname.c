61c4666296aa93f62217900a92c648153d417b05
realtalishaw <misstalishawhite@gmail.com>
2020-10-11 00:52:04 +0000

    It ain't waht they call you

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - fdsfsd
+ *
+ * @__attribute__((unused)): fgklsfgs
+ * @argv: fdsgdgs
+ * Return: gfss
+ */
+
+int main(int argc __attribute__((unused)); char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
