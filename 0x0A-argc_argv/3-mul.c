4ffc6e5924c5a7c48f2987a77bacda11d17c6ce2
realtalishaw <misstalishawhite@gmail.com>
2020-10-11 17:34:07 +0000

    syntax issues insue

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - fds
+ * @argc: fds
+ * @argv: fdsfd
+ *
+ * Return: fdasd
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc == 3)
+	{
+		printf("%d\n",(atoi(argv[1]) * (atoi(argv[2]))));
+		else
+		{
+			printf("Error\n");
+		}
+	}
+			return (0);
+}
