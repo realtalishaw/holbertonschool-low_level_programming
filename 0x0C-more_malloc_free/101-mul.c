e4fab9c4693b6ce5e15dca6bd1c0a2dceb0f2df2
realtalishaw <misstalishawhite@gmail.com>
2020-10-15 19:59:00 +0000

    added an error condition

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,6 +1,7 @@
 #include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
  * main - fds
@@ -13,7 +14,14 @@
 int main(int argc, char *argv[])
 {
 	if (argc == 3)
+	{
+		if (!(isdigit(argv[1][2])))
+		{
+			printf("Error\n");
+			exit (98);
+		}
 		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	}
 	else
 	{
 		printf("Error\n");
