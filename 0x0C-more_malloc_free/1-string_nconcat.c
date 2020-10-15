e16505616b082bb6927c737569a7ae0e2ac23366
realtalishaw <misstalishawhite@gmail.com>
2020-10-15 20:54:16 +0000

    testing

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,7 +15,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *both;
 	int i;
-	unsigned int j, len;
+	unsigned int j, len, len2;
 
 
 	if (s1 == NULL)
@@ -25,6 +25,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 
 	for (len = 0; s2[len]; len++)
+		;
+	for (len2 = 0; s1[len2]; len2++)
 		;
 
 	if (len > n)
