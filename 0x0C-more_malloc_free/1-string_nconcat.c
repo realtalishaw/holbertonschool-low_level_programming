255994a6ec488220702b14d0e2003944f9613e75
realtalishaw <misstalishawhite@gmail.com>
2020-10-15 20:11:21 +0000

    added a null ending

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -27,7 +27,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (len = 0; s1[len] != 0; len++)
 		;
 
-	both = malloc(sizeof(char) * (len + n + 1));
+	both = malloc(*s1 + (len + n + 1));
 
 	if (both == NULL)
 		return (NULL);
@@ -35,7 +35,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; s1[i] != 0; i++)
 		both[i] = s1[i];
 
-	for (j = 0; j <= n; j++)
+	for (j = 0; s2[j] && j <= n; j++)
 		both[i + j] = s2[j];
 
 	both[i + j] = '\0';
