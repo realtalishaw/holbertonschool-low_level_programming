3cad78ec59cafcb4b99bb0e26e79b1e193b37658
realtalishaw <misstalishawhite@gmail.com>
2020-10-15 19:38:33 +0000

    multiply

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - fds
+ * @argc: fds
+ * @argv: ddfd
+ *
+ * Return: dsa
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc == 3)
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	else
+	{
+		printf("Error\n");
+	}
+	return (0);
+}
