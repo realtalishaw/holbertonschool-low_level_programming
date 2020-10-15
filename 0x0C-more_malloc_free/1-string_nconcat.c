dd4e595d442c7492d684771a342714b8fde6ae55
realtalishaw <misstalishawhite@gmail.com>
2020-10-15 20:34:56 +0000

    s2 string len and more

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,7 +15,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *both;
 	int i;
-	unsigned int j, len;
+	unsigned int j, len, len2;
 
 
 	if (s1 == NULL)
@@ -26,13 +26,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	for (len = 0; s2[len]; len++)
 		;
+	for (len2 = 0; s1[len2]; len2++)
+		;
 
 	if (n > len)
 	{
 		len = n;
 	}
 
-	both = malloc(*s1 + (len + 1));
+	both = malloc(len2 + (len + 1));
 
 	if (both == NULL)
 		return (NULL);
