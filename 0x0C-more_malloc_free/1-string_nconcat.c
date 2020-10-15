4bf8d68c7ccd280af890b0d800d71da641527b2b
realtalishaw <misstalishawhite@gmail.com>
2020-10-15 18:07:15 +0000

    _realloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -31,7 +31,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; s1[i] != 0; i++)
 		both[i] = s1[i];
 
-	both = realloc(both, sizeof(char) * n);
+	both = _realloc(both, sizeof(char) * n);
 
 	if (both == NULL)
 		return (NULL);
