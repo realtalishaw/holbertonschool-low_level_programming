46ed18e56166da8c46dc7f66fd49cd7e0ff60e40
realtalishaw <misstalishawhite@gmail.com>
2020-10-15 17:41:07 +0000

    void

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _calloc - fdsgfd
+ * @nmemb: fdsfds
+ * @size: fdsd
+ *
+ * Return: fdkfds
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *new;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	new = malloc(nmemb * size);
+
+	if (new == NULL)
+		return (NULL);
+
+	if (new)
+	{
+		for (i = 0; i < (nmemb * size); i++)
+		{
+			new[i] =  '\0';
+		}
+	}
+
+	return (new);
+}
