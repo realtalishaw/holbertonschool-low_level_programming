36e1e3a8ab2db14404a57df78d5200c21675620c
realtalishaw <misstalishawhite@gmail.com>
2020-10-15 23:34:08 +0000

    header file

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -7,5 +7,6 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 char *_memcpy(char *dest, char *src, unsigned int n);
-
+int _printf(const char *format, ...);
+int _atoi(char* str);
 #endif
