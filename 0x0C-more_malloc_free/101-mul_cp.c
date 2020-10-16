7ca81f4c6a76810482da84e7b734fd65e9b34e1c
realtalishaw <misstalishawhite@gmail.com>
2020-10-16 18:22:28 -0500

    Update 101-mul_cp.c

--- 0x0C-more_malloc_free/101-mul_cp.c
+++ 0x0C-more_malloc_free/101-mul_cp.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-
 /**
  * main - fds
  * @argc: fds
@@ -10,92 +9,100 @@
  *
  * Return: dsa
  */
-
 int main(int argc, char *argv[])
 {
-	int i;
-
-	for (i = 0; argv[1][i] != 0; i++)
-	{
-		if (_isdigit(argv[1][i]) == 0)
-		{
-			printf("Error\n");
-			exit(98);
-		}
-	}
-
-	for (i = 0; argv[2][i] != 0; i++)
-	{
-		if (_isdigit(argv[2][i]) == 0)
-		{
-			printf("Error\n");
-			exit(98);
-		}
-	}
-	if (argc == 3)
-	{
-		if (argv[2][i] > 20)
-		{
-			mul(argv[1], argv[2]);
-		}
-		 printf("%d\n", _atoi(argv[1]) * _atoi(argv[2]));
-	}
-
-	else
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	return (0);
+    int i;
+    for (i = 0; argv[1][i] != 0; i++)
+    {
+       
+        if (_isdigit(argv[1][i]) == 0)
+        {
+            printf("Error\n");
+            exit(98);
+        }
+    }
+    for (i = 0; argv[2][i] != 0; i++)
+    {
+        if (_isdigit(argv[2][i]) == 0)
+        {
+            printf("Error\n");
+            exit(98);
+        }
+    }
+    if (argc == 3)
+    {
+        
+        if (i > 10)
+        {
+            printf("Number is Huge!\n");
+            mul(argv[1], argv[2]);
+        }
+         printf("%d\n", _atoi(argv[1]) * _atoi(argv[2]));
+    }
+    else
+    {
+        printf("Error\n");
+        exit(98);
+    }
+    return (0);
 }
-
 /**
  * _atoi - fdss
  * @str: dss
  *
  * Return: dsd
  */
-
 int _atoi(char *str)
 {
-	int res = 0;
-	int i;
-
-	for (i = 0; str[i] != 0; ++i)
-		res = res * 10 + str[i] - '0';
-
-	return (res);
+    \
+    int res = 0;
+    int i;
+    for (i = 0; str[i] != 0; ++i)
+        res = res * 10 + str[i] - '0';
+    return (res);
 }
-
 /**
  * _isdigit - dasdsa
  * @c:fds
  *
  * Return: sdasdads
  */
-
 int _isdigit(int c)
 {
-	return (c >= '0' && c <= '9');
+    return (c >= '0' && c <= '9');
 }
-
-char *mul(char *num1, char *num2)
+void *mul(char *num1, char *num2)
 {
-	int len2;
-	int i = 0;
-  	size_t sum;
-	void *ptr;
+    int len, len2, i, j, x, y;
+    size_t sum;
+    int *ptr;
 
-	for (len2 = 0; num2[len2]; len2++)
-		;
+    for (len2 = 0; num2[len2]; len2++)
+        ;
 
-	while(num2[len2]--)
-	{
-		sum += num1[i] * num2[len2];
-		num2 += num2[len2] * 10;
-		i++;
-	}
-	ptr = malloc(sum);
+    for (len = 0; num2[len]; len++)
+        ;
 
-	return (ptr);
+    for (i = (len2 -1); i >= 0; i--)
+    {
+        x = num2[i];
+        for (j = (len - 1); j >= 0; j-- && x * 10)
+        {
+            y = num1[j];
+            sum += (x * y);
+        }
+    }
+    ptr = malloc(sizeof(sum));
+    for
+    if (ptr == NULL)
+    {
+        free(ptr);
+        return (NULL);
+    }
+    while(ptr++)
+    {
+        putchar(*ptr + '0');
+    }
+    free(ptr);
+    return (0);
 }
