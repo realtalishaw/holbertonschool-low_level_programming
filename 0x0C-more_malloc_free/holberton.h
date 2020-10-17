782b49356f09a087c1ebf73e04a1dc65bf8cef46
realtalishaw <misstalishawhite@gmail.com>
2020-10-17 14:34:52 +0000

    updated header

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -1,6 +1,8 @@
 #ifndef Holberton_H
 #define HOLBERTON_H
 
+int _putchar(char c)
+int _strlen(char *s)
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
@@ -10,5 +12,6 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 int _printf(const char *format, ...);
 int _atoi(char* str);
 int _isdigit(int c);
+char *mul(char *num1, char *num2);
 
 #endif
