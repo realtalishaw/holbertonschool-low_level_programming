8e056f74deb9306232447bdf488cd1793a98148f
realtalishaw <misstalishawhite@gmail.com>
2020-10-17 10:37:16 -0500

    Update holberton.h

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -1,8 +1,8 @@
 #ifndef Holberton_H
 #define HOLBERTON_H
 
-int _putchar(char c)
-int _strlen(char *s)
+int _putchar(char c);
+int _strlen(char *s);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
