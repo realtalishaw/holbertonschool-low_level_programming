89da9d46bfe160da8b1bb32f518c46ad283465cd
realtalishaw <misstalishawhite@gmail.com>
2020-10-19 20:00:27 +0000

    line breaks

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -25,10 +25,17 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		d->name = _strdup(name);
 		if (!d->name)
-			free(d), return (NULL);
+		{
+			free(d);
+			return (NULL);
+		}
 		d->owner = _strdup(owner);
 		if (!d->owner)
-			free(d->name), free(d), return (NULL);
+		{
+			free(d->name);
+			free(d);
+			return (NULL);
+		}
 		d->age = age;
 	}
 
