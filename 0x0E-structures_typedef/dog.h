a44b3c931ef160662d907bf5bb858340c85c2344
realtalishaw <misstalishawhite@gmail.com>
2020-10-19 16:42:20 +0000

    added #3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,14 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif
