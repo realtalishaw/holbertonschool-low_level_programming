2805cb7917a83d109207291cf25c51e916b45fbf
realtalishaw <misstalishawhite@gmail.com>
2020-10-19 18:14:30 +0000

    get init

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - sdka
+ * @d: ssadsada
+ * @name: dkdjs
+ * @age: dasfs
+ * @owner: dskajafj
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
