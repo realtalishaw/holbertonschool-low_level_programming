93e30e77092e7955d7f76d8f38627774901b8af2
realtalishaw <misstalishawhite@gmail.com>
2020-10-21 14:09:37 +0000

    that IS the answer

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - fjdkjsf
+ * @name: fjkds
+ * @f: fjds
+ *
+ * Return: dssds
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
