258d743746c3c138be8ad81301105e4b29d56c8d
realtalishaw <misstalishawhite@gmail.com>
2020-10-21 15:03:24 +0000

    for loops saved my life

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * array_iterator - fdds
+ * @array: fdsds
+ * @size: fdssd
+ * @action: fdsds
+ *
+ * Return: fds
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+	for(i = 0; i < size; i++)
+		action(array[i]);
+}
