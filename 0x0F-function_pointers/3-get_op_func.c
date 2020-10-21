c807e25371cf4a3c31c1f16b1a7e97d8d33fbb8e
realtalishaw <misstalishawhite@gmail.com>
2020-10-21 21:12:44 +0000

    Betty

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,31 @@
+#include "3-calc.h"
+#include <stdio.h>
+
+/**
+ * get_op_func - fsfsd
+ * @s: jfgkjgdk
+ *
+ * Return: fjdkfjds
+ */
+
+int (*get_op_func(char *s))(int a, int b)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+
+	while (ops[i].op != NULL && ops[i].op != s)
+	{
+		i++;
+	}
+
+	return (ops[i].f);
+}
