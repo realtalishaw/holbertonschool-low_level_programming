eb7a52ba6859a9b2002bab262e7e3cf655566bcf
realtalishaw <misstalishawhite@gmail.com>
2020-10-21 22:38:22 +0000

    ==

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	if (argv[3][0] != 0 && (argv[2][0] != '/' || argv[2][0] != '%'))
+	if (argv[3][0] != 0 && (argv[2][0] == '/' || argv[2][0] == '%'))
 	{
 		printf("Error\n");
 		exit(100);
