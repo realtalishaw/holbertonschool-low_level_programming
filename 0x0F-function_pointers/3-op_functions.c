044338d1213cec3305a75e665c931e9f3ae7b285
realtalishaw <misstalishawhite@gmail.com>
2020-10-21 21:13:21 +0000

    Betty

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+#include "3-calc.h"
+
+/**
+ * op_add - fkdsd
+ * @a: fdjfsd
+ * @b: lfff
+ *
+ * Return: fjdksjs
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - fdfsdfs
+ * @a: fkjksfjds
+ * @b: fjdlksds
+ *
+ * Return: fdjksdjfs
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - fddsfsd
+ * @a: fdsdf
+ * @b: dnkfjsd
+ *
+ * Return: fdsfds
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - jfkjkds
+ * @a: fjdkljfds
+ * @b: kfdksdfds
+ *
+ * Return: fkjs
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - dffdfs
+ * @a: gfdgdf
+ * @b: fhdjsjhfljkds
+ *
+ * Return: fjlskdjfs
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
