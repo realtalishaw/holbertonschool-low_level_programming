16c5d763e2bd21fd69b3a9c33242f63cb2a67eac
realtalishaw <misstalishawhite@gmail.com>
2020-10-21 16:24:20 +0000

    return values matter

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,31 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - dhklfjdklfsd
+ * @array: fdkjfsklfsd
+ * @size: fdhfjkfg
+ * @cmp: jfkgdf
+ *
+ * Return: fjdkljfs
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (!cmp)
+		return (0);
+
+	if (size <= 0)
+		return (-1);
+	if (cmp)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+		}
+	}
+
+	return (-1);
+}
