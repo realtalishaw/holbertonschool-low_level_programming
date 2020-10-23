41e54c06b8c19a6c07242110af710ce07216d2c8
realtalishaw <misstalishawhite@gmail.com>
2020-10-23 15:04:47 +0000

    added the condition

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -12,15 +12,22 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list strings;
 	unsigned int i;
+	char *p;
 
 	va_start(strings, n);
 
 	if (!separator)
 		separator = "";
 
+
+
+
 	for (i = 0; i < n; i++)
 	{
-		printf("%s", va_arg(strings, char *));
+		p = va_arg(strings, char *);
+		if (!p)
+			p = "(nil)";
+		printf("%s", p);
 		if (i != n - 1)
 			printf("%s", separator);
 	}
