c07b846d4250bc176f7928f90d8670498fb969f8
realtalishaw <misstalishawhite@gmail.com>
2020-11-04 14:37:10 +0000

    adding nodes

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - gfdf
+ * @head: fds
+ * @n: fdsdfs
+ *
+ * Return: fsfgs
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *newnode;
+
+	newnode = malloc(sizeof(listint_t));
+
+	if (!newnode)
+		return (NULL);
+
+	newnode->n = n;
+	newnode->next = *head;
+	*head = newnode;
+
+	return (newnode);
+}
