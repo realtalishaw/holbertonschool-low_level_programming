373a21cde8f6d90ad0808767e57388d755f33b2e
realtalishaw <misstalishawhite@gmail.com>
2020-11-04 14:42:20 +0000

    free the homie.... again

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - gdsfd
+ * @head: fsfds
+ *
+ * Return: fdsdfs
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *t;
+	listint_t *t2;
+
+	t2 = *head;
+
+	while (t2 && head)
+	{
+		t = t2;
+		t2 = t2->next;
+		free(t);
+	}
+	*head = NULL;
+}
