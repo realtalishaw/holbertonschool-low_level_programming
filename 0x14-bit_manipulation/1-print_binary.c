72e6de3ca0011c0b8f893b5546ebfef6359d2b57
realtalishaw <misstalishawhite@gmail.com>
2020-11-05 14:22:04 +0000

    1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * print_binary - fdfdg
+ * @n: fjkgljfd
+ *
+ * Return: fjhdsgs
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int range = 2147483648;
+
+	if (n == 0)
+		_putchar ('0');
+
+xy:
+	if (range > 0)
+	{
+		if ((n & range) == 0)
+		{
+			range = range>>1;
+			goto xy;
+		}
+		else
+			while (range > 0)
+			{
+				if ((n & range) == 0)
+					_putchar('0');
+				else
+					_putchar('1');
+				range = range >> 1;
+			}
+	}
+}
