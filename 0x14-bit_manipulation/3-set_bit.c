59d71901212b99b053eb8f228b7a314754fb9a7c
realtalishaw <misstalishawhite@gmail.com>
2020-11-05 14:48:49 +0000

    oh, well that worked

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * set_bit - jfdkjfsd
+ * @n: fjdlsjfklds
+ * @index: gjkflfls
+ *
+ * Return: dlfjlfkds
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	*n = *n | 1 << index;
+
+	return (1);
+}
