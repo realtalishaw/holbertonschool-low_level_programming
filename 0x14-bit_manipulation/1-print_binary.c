9c4b936f4adede511503674caa5a40cb1a4f352d
realtalishaw <misstalishawhite@gmail.com>
2020-11-05 23:43:39 +0000

    checking for checks

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,27 +9,27 @@
 
 void print_binary(unsigned long int n)
 {
-	unsigned long int range = 2147483648;
+	unsigned long int i = 2147483648;
 
 	if (n == 0)
 		_putchar ('0');
 
 xy:
-	if (range > 0)
+	if (i > 0)
 	{
-		if ((n & range) == 0)
+		if ((n & i) == 0)
 		{
-			range = range >> 1;
+			i = i >> 1;
 			goto xy;
 		}
 		else
-			while (range > 0)
+			while (i > 0)
 			{
-				if ((n & range) == 0)
+				if ((n & i) == 0)
 					_putchar('0');
 				else
 					_putchar('1');
-				range = range >> 1;
+				i = i >> 1;
 			}
 	}
 }
