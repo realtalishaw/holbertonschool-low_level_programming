970239d5b9a7238bf524146bf378effea5558885
realtalishaw <misstalishawhite@gmail.com>
2020-11-05 23:49:33 +0000

    trying to save my checks

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,35 +1,32 @@
 #include "holberton.h"
-
 /**
  * print_binary - fdfdg
  * @n: fjkgljfd
  *
  * Return: fjhdsgs
  */
-
 void print_binary(unsigned long int n)
 {
-	unsigned long int i = 2147483648;
+	unsigned long int range = 2147483648;
 
 	if (n == 0)
 		_putchar ('0');
-
 xy:
-	if (i > 0)
+	if (range > 0)
 	{
-		if ((n & i) == 0)
+		if ((n & range) == 0)
 		{
-			i = i >> 1;
+			range = range >> 1;
 			goto xy;
 		}
 		else
-			while (i > 0)
+			while (range > 0)
 			{
-				if ((n & i) == 0)
+				if ((n & range) == 0)
 					_putchar('0');
 				else
 					_putchar('1');
-				i = i >> 1;
+				range = range >> 1;
 			}
 	}
 }
