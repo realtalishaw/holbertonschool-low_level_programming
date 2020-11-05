010650e486a5db0f7ee8057e6387cce66b0aa7e1
realtalishaw <misstalishawhite@gmail.com>
2020-11-05 14:53:00 +0000

    perfect!

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * clear_bit - fedklfak
+ * @n: fhjdjs
+ * @index: hjfgjd
+ *
+ * Return: dsfjdsfs
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (!n)
+		return (-1);
+
+	*n = *n & ~(1 << index);
+	return (1);
+}
