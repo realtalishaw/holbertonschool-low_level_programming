0107e5a994e9ddc08792943ae457cda009bbac4c
realtalishaw <misstalishawhite@gmail.com>
2020-11-05 14:38:27 +0000

    10

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * get_bit - gfdgfdd
+ * @n: fdfsds
+ * @index: fjklljs
+ *
+ * Return: fjdkfsk
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned result;
+
+	result =  (n >> index) & 1;
+
+	return (result);
+}
