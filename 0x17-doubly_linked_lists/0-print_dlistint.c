811ab284edea25515b606f7846d9836d53258d1a
realtalishaw <misstalishawhite@gmail.com>
2020-12-21 14:38:51 +0000

    Print List

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - fjklsjds
+ * @h: fks
+ *
+ * Return: djksds
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	int i = 0;
+	const dlistint_t *list = h;
+
+	if (!h)
+		return (i);
+	else
+	{
+		while (list)
+		{
+			printf("%i\n", list->n);
+			list = list->next;
+			i++;
+		}
+	}
+	return (i);
+}
