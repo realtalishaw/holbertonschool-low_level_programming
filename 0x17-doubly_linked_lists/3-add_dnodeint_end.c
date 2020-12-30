c803846ed5b72ef387940a8318652e4ce061fd2c
realtalishaw <misstalishawhite@gmail.com>
2020-12-30 14:00:09 +0000

    this is almost exactly like singly linked lists

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - fds
+ * @head: fdf
+ * @n: fdfd
+ *
+ * Return: fsfs
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *newnode;
+	dlistint_t *tmpnode;
+
+	newnode = malloc(sizeof(dlistint_t));
+
+	if(!newnode)
+		return (NULL);
+
+	newnode->n = n;
+	newnode->next = NULL;
+
+	if(!*head)
+	{
+		newnode->prev = NULL;
+		*head = newnode;
+	}
+	else
+	{
+		tmpnode = *head;
+		while(tmpnode->next)
+			tmpnode = tmpnode->next;
+		tmpnode->next = newnode;
+		newnode->prev = tmpnode;
+	}
+	return (newnode);
+}
