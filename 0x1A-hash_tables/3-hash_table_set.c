f235429bb51fb37232d2db8af784925ef5123248
realtalishaw <misstalishawhite@gmail.com>
2021-01-15 22:26:58 +0000

    please

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -17,7 +17,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (!ht)
 		return (0);
-	if (!key || strcmp(key, ""))
+	if (!key || strcmp(key, "") == 0)
+		return (0);
+	if (ht->size == 0)
 		return (0);
 	idx = key_index((unsigned char *)key, ht->size);
 	if (ht->array[idx])
