0676088d9c196f28c8a257a29f82855c2694a458
realtalishaw <misstalishawhite@gmail.com>
2021-01-15 22:17:02 +0000

    removed 0

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -19,8 +19,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	if (!key || strcmp(key, ""))
 		return (0);
-	if (ht->size == 0)
-		return (0);
 	idx = key_index((unsigned char *)key, ht->size);
 	if (!ht->array[idx])
 	{
