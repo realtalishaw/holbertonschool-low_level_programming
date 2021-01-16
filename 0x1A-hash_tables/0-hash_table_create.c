c422c45ede1629b13a561ab5b6fc2b5c1af71b7c
realtalishaw <misstalishawhite@gmail.com>
2021-01-16 01:19:54 +0000

    removed a star

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -20,7 +20,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (!new)
 		return (NULL);
 
-	new->array = malloc(sizeof(hash_node_t *) * size);
+	new->array = malloc(sizeof(hash_node_t) * size);
 
 	if (!new->array)
 		return (NULL);
