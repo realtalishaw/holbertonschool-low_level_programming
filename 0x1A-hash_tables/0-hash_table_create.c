e3298074feb65d7b98f4222637d2476c25620749
realtalishaw <misstalishawhite@gmail.com>
2021-01-16 01:28:01 +0000

    assignment

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -20,13 +20,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (!new)
 		return (NULL);
 
-	new->array = malloc(sizeof(hash_node_t) * size);
-
+	new->array = malloc(sizeof(hash_node_t *) * size);
+	new->size = size;
 	if (!new->array)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
-		new->array[i] = NULL;
+		new->array[i] = 0;
 
 	return (new);
 }
